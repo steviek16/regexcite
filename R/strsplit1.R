6ec4940c91537a905311410de376c1547baa0eec
Stephen Kinsey <ichirokinsey@gmail.com>
2025-10-11 11:03:37 -0400

    Add roxygen2 documentation and install working package

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -3,14 +3,14 @@
 #' simple helper function that splits a single string into its parts, always returns a character vector
 #' it is designed for the common case where the input is "morally" a scalar string
 
-#'@param x A character vecto of length one (a single string)
-#'@param split A character string to use as the delimiter
-#'@return A character vector of the split components
+#' @param x A character vecto of length one (a single string)
+#' @param split A character string to use as the delimiter
+#' @return A character vector of the split components
 
-#'@examples
-#'strsplit1("alfa,bravo,charlie,delta", ",")
+#' @examples
+#' strsplit1("alfa,bravo,charlie,delta", ",")
 
-#'@export
+#' @export
 strsplit1 <- function(x, split) {
   strsplit(x, split=split)[[1]]
 
